322ddf432a3c5ac9cf57b96fc148ae49c22d20e3
RtyXmd <573850993@qq.com>
2018-11-30 17:27:40 +0800

    添加radio组件

--- src/example/rtyRadio.vue
+++ src/example/rtyRadio.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
-    <rty-radio></rty-radio>
+    <rty-radio
+      v-for="(i,ind) in radioList"
+      :key="ind"
+      v-model="radio"
+      @change="getRadio"
+      :label="i.value"
+    >{{i.name}}</rty-radio>
   </div>
 </template>
 
 <script>
-  export default {
-    
+export default {
+  data() {
+    return {
+      radio: 0,
+      radioList: [
+        {
+          value: 0,
+          name: "选项A"
+        },
+        {
+          value: 1,
+          name: "选项B"
+        },
+        {
+          value: 2,
+          name: "选项C"
+        },
+        {
+          value: 3,
+          name: "选项D"
+        }
+      ]
+    };
+  },
+  methods: {
+    getRadio(val) {
+      console.log(this.radio);
+      console.log(val);
+    }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
